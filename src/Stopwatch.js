22caf4f6f0f8b2d2a82c11e8f8b420f2ba102cf7
Anthony DiRusso <ossurid@gmail.com>
2019-04-28 06:45:52 -0400

    uses Mat.floor to divide timerTime up into hour minutes seconds

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -3,6 +3,8 @@ import React from 'react'
 function Stopwatch({clickHandler, timerBool, timerTime}) {
   return (<>
             <h2>{Math.floor(timerTime/3600)} Hours</h2>
+            <h3>{Math.floor(timerTime/60)%60} Minutes</h3>
+            <h4>{timerTime%60} Seconds</h4>
             <button onClick={clickHandler}>{timerBool ? `Stop` : `Start`}</button>
           </>     
   )
