2506643142263711dd40e8b3103ce1ccfee017be
Anthony DiRusso <ossurid@gmail.com>
2019-04-28 06:28:47 -0400

    adds Stopwatch.js and links it with App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import './App.css';
+import Stopwatch from './Stopwatch';
+
 
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tickingTime: 0,
+      tickTickTick: false
+    };
+  }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-
+          <Stopwatch tick={this.state.tickingTime}/>
         </header>
       </div>
     );
---
+++ src/Stopwatch.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Stopwatch(props) {
+  return (
+    <div>
+      <h2>{props.tick}</h2>
+    </div>
+  )
+}
+
+export default Stopwatch
